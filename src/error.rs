79ce0545227e4cbe8ae3c6b3a953a3b02edcdcf3
Shun Sambongi <shunsambongi@gmail.com>
2022-03-24 00:52:10 -0600

    add convenience method to construct api error

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use thiserror::Error;
 
+/// Errors which may occur when using API endpoints.
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum ApiError<E>
@@ -14,4 +15,14 @@ where
         /// The client error.
         source: E,
     },
+}
+
+impl<E> ApiError<E>
+where
+    E: Error + Send + Sync + 'static,
+{
+    /// Create an API error from a client specific error.
+    pub fn client(source: E) -> Self {
+        ApiError::Client { source }
+    }
 }
